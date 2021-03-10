9d55f937c02c1c04cd498dd9bfec31e0ef13ad8a
Sylvio Miguel <sylviomiguel@outlook.com>
2021-03-10 19:28:54 -0300

    Mudanças4

--- Gerenciadores/GerenciFut.py
+++ Gerenciadores/GerenciFut.py
@@ -1,20 +1,46 @@
 #Aqui temos um gerenciador para um técnico por exemplo, para avaliar o jogador com maior aproveitamento (;
 #!/usr/bin/env python3
+time = list()
 dados = dict()
 partidas = list()
-dados ['nome'] = str(input('Nome do jogador: '))
-tot = int(input(f'Quantas partidas {dados["nome"]} jogou ?'))
-for p in range(tot):
-    partidas.append(int(input(f'Quantos gols na partida {p+1}: ')))
-dados['gols'] = partidas [:]
-dados ['total'] = sum(partidas)
+while True:
+    dados.clear()
+    dados ['nome'] = str(input('Nome do jogador: '))
+    tot = int(input(f'Quantas partidas {dados["nome"]} jogou ?'))
+    partidas.clear()
+    for p in range(tot):
+        partidas.append(int(input(f'Quantos gols na partida {p+1}: ')))
+    dados['gols'] = partidas [:]
+    dados ['total'] = sum(partidas)
+    time.append(dados.copy())
+    while True:
+        resp = str(input('Quer continuar ? [S/N]')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Por favor, digite apenas S ou N')
+    if resp == 'N':
+        break
 print('=-'*30)
-print(dados)
-print('=-'*30)
-for k,v in dados.items():
-    print(f'O campo {k} tem o valor {v}')
-print('=-'*30)
-print(f'O jogador {dados["nome"]} jogou {tot} partidas')
-for i,v in enumerate(dados['gols']):
-    print(f'  =>Na partida {i+1} marcou {v} gols')
-print(f'Foi um total de {dados["total"]} gols')
+print('cod ',end='')
+for i in dados.keys():
+    print(f'{i:<15}',end='')
+print()
+print('-'*40)
+for k,v in enumerate(time):
+    print(f'{k:>3} ',end='')
+    for d in v.values():
+        print(f'{str(d):<15}',end='')
+    print()
+print('-'*30)
+while True:
+    busca = int(input('Mostrar dados de qual jogador ? (999 para parar) '))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'ERRRO! Não existe jogador com código {busca}!')
+    else:
+        print(f' -- LEVANTAMENTO do JOGADOR {time[busca]["nome"]}')
+        for i,g in enumerate(time[busca]['gols']):
+            print(f'   No jogo {i+1} fez {g} gols.')
+    print('=-'*30)
+print('<<< VOLTE SEMPRE >>>')
